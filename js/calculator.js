7944284316d6e36a2972f952240cacd7c7f8fa44
Aleksandr <alperovich_a@mail.ru>
2024-09-25 15:44:52 +0300

    1

---
+++ js/calculator.js
@@ -0,0 +1,11 @@
+function calculateBMI() {
+    var weight = parseFloat(document.getElementById(«weight»).value);
+    var height = parseFloat(document.getElementById(«height»).value) / 100; // переводим см в метры
+    var bmi = weight / (height * height);
+    var result = document.getElementById(«result»);
+    if (!isNaN(bmi)) {
+    result.innerHTML = «Your BMI is » + bmi.toFixed(2);
+    } else {
+    result.innerHTML = «Please enter valid values.»;
+    }
+    }
