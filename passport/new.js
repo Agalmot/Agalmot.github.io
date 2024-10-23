b98fe26997a3bdca2bbc8d613d8555c189c000b0
Aleksandr <alperovich_a@mail.ru>
2024-10-23 16:48:49 +0300

    fgh

--- passport/new.js
+++
@@ -1,9 +0,0 @@
-const node_for_click = ("for_click")
-
-function find_edit(){
-    const item  = document.getElementById('new')
-    console.log(item.innerText)
-    item.innerHTML="<b>новый фрагмент</b>"
-}
-
-node_for_click.addEventListener("click",find_edit)
